7920f227afe8ad39a34279346f165e4e039b05e4
Mohandhass <mohandhass28@gmail.com>
2024-01-02 15:01:32 +0530

    new

--- src/canvas/Backdrop.jsx
+++ src/canvas/Backdrop.jsx
@@ -15,20 +15,19 @@ const Backdrop = () => {
       rotation={[Math.PI / 2, 0, 0]}
       position={[0, 0, -0.14]}
     >
-      <RandomizedLight
+     <RandomizedLight 
         amount={4}
         radius={9}
         intensity={0.55}
         ambient={0.25}
         position={[5, 5, -10]}
       />
-
-      <RandomizedLight
-        amount={6}
-        radius={10}
-        intensity={1}
-        ambient={0.25}
-        position={[4, 5, 0]}
+      <RandomizedLight 
+        amount={4}
+        radius={5}
+        intensity={0.25}
+        ambient={0.55}
+        position={[-5, 5, -9]}
       />
     </AccumulativeShadows>
   );
